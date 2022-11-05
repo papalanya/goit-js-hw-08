ab50e51a9d3aa0a2e125cd38034915e78d0255c9
Illya Pavlenko <illiampavlenko@gmail.com>
2022-11-05 17:26:18 +0200

    add task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(saveTime, 1000));
+
+if (localStorage.getItem('videoplayer-current-time')) {
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));    
+}
+
+function saveTime(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+}
