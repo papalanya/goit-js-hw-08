e55b40ee9440e95e71ce4e03ae8eff39f3a83ba6
Illya Pavlenko <illiampavlenko@gmail.com>
2022-11-05 19:20:53 +0200

    add task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('textarea');
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {};
+
+form.addEventListener('input', throttle(onSave, 500));
+form.addEventListener('submit', onFormSubmit);
+
+populateInput();
+
+function onSave(event) {
+    event.preventDefault();
+    
+    const emailInput = form.elements.email.value;
+    const messageInput = textarea.value;
+
+    formData = {
+        email: emailInput, message: messageInput
+    };
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateInput() {
+    const saveInput = localStorage.getItem(STORAGE_KEY);
+
+    if (saveInput) {
+        const parsSaveInput = JSON.parse(saveInput);
+        textarea.value = parsSaveInput.message || '';
+        form.nextElementSibling.email.value = parsSaveInput.email || '';
+    }
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const saveInput = localStorage.getItem(STORAGE_KEY);
+
+    if (saveInput) {
+        const parsSaveInput = JSON.parse(saveInput);
+        console.log(parsSaveInput);
+    }
+
+    localStorage.removeItem(STORAGE_KEY);
+
+    event.currentTarget.reset();
+}
